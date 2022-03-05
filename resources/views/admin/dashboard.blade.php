b37c4d2baab0c4c500321335390ff157c841d1c7
KaisarJamil <kaisar.mistrisolutions@gmail.com>
2022-03-06 01:36:11 +0600

    first commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.backend.backend.app')
+
+@section('title','Dashboard')
+
+
+@push('css')
+@endpush
+
+@section('content')
+    <p>this is dashboard</p>
+@endsection
+
+@push('js')
+@endpush
+
